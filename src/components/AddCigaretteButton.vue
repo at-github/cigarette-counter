6f0f631b8de9f0473b928e1730fb289679e0e688
Tarik Amar <tarik.amar@gmail.com>
2023-09-12 08:13:08 +0000

    Core - Clean unnecessaries component and define the main one

---
+++ src/components/AddCigaretteButton.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+  <div class="greetings">
+    <button>{{ msg }}</button>
+  </div>
+</template>
+
+<style scoped>
+</style>
